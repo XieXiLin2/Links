3bddca2700c8ffb61abcfcd516639d3107247372
XieXiLin <support@xiexilin.com>
2024-02-15 14:13:40 +0800

    upd: Souiken

--- generate.py
+++ generate.py
@@ -1,1 +1,23 @@
 from jinja2 import Template
+
+from pydantic import BaseModel, parse_file_as
+from pydantic.networks import AnyHttpUrl
+from typing import List
+
+
+class FriendLink(BaseModel):
+    name: str
+    url: AnyHttpUrl
+
+
+class Friend(BaseModel):
+    avatar: AnyHttpUrl
+    name: str
+    banner: str
+    links: List[FriendLink]
+
+
+def main():
+    friends = parse_file_as(List[Friend], "links.json")
+    template = Template(open("template.html").read())
+    template.
