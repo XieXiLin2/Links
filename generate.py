76220702eb89980a5a50aaea2ce0d0473486edd0
FalfaChino <falfachino@qq.com>
2024-05-10 20:00:11 +0800

    😅 featL render

--- generate.py
+++ generate.py
@@ -1,25 +1,36 @@
-# Uncompleted #
-
 from jinja2 import Template
 
-from pydantic import BaseModel, parse_file_as
+from pydantic import BaseModel
 from pydantic.networks import AnyHttpUrl
-from typing import List
+from typing import Annotated
+
+from cookit.pyd import type_validate_json
+
+from pathlib import Path
+
+LINK_JSON = Path().cwd() / "links.json"
+INDEX_HTML = Path().cwd() / "index.html"
+JINJA2_TEMPLATE = Path().cwd() / "index.jinja2"
 
 
 class FriendLink(BaseModel):
     name: str
-    url: AnyHttpUrl
+    link: Annotated[AnyHttpUrl, str]
 
 
 class Friend(BaseModel):
-    avatar: AnyHttpUrl
+    avatar: Annotated[AnyHttpUrl, str]
     name: str
     banner: str
-    links: List[FriendLink]
+    links: list[FriendLink]
 
 
 def main():
-    friends = parse_file_as(List[Friend], "links.json")
-    template = Template(open("template.html").read())
-    template.
+    friends = type_validate_json(list[Friend], LINK_JSON.read_bytes())
+    template = Template(JINJA2_TEMPLATE.read_text(encoding="UTF-8"))
+    render = template.render(links=friends).encode("UTF-8")
+    print(INDEX_HTML.write_bytes(render))
+
+
+if __name__ == "__main__":
+    main()
